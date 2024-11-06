17c432283b3c65d07a6b7d4d3ff91190694e344b
Ruslan Vanzhula <ruslan.vanzhula@ideasoft.io>
2024-11-06 15:16:24 +0200

    replace express with fastify

--- apps/gateway-service/src/main.ts
+++ apps/gateway-service/src/main.ts
@@ -1,4 +1,8 @@
 import { NestFactory } from '@nestjs/core';
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
 import { Logger } from 'nestjs-pino';
 
 import { AppModule } from './app.module';
@@ -8,15 +12,19 @@ import { GrpcExceptionInterceptor } from '@app/libs/grpc-client/grpc-exception.i
 import { AjvValidationPipe } from '@app/libs/validation';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, {
-    bufferLogs: true,
-  });
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter(),
+    {
+      bufferLogs: true,
+    },
+  );
 
   app.useGlobalPipes(new AjvValidationPipe());
   app.useGlobalInterceptors(new GrpcExceptionInterceptor());
   app.useLogger(app.get(Logger));
   setupSwagger(app);
 
-  await app.listen(3000);
+  await app.listen(3000, '0.0.0.0');
 }
 bootstrap();
